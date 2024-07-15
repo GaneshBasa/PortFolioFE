b5168020551fa0fb6a8ddf45051b2a0d4420bb79
Ganesh Basa <basaganesh1@gmail.com>
2024-07-15 18:43:58 +0530

    Added Interfaces

--- src/common/interfaces.ts
+++ src/common/interfaces.ts
@@ -2,3 +2,21 @@ import { PropsWithChildren } from 'react'
 
 
 export interface RootLayoutProps extends PropsWithChildren {}
+
+
+export interface NavLinkProps extends PropsWithChildren {
+  target: string
+  active?: boolean
+  className?: string
+}
+
+
+export interface BaseAppState {
+  activeSection: string | null
+}
+
+
+export interface AppState {
+  appState: BaseAppState
+  updateState: ( newAppState: Partial<BaseAppState> ) => void
+}
