8d73b536116959f5488133533f5f5f8f0620a8d6
Ganesh Basa <basaganesh1@gmail.com>
2024-07-29 10:39:54 +0530

    Modified Color Hook Default Theme

--- src/hooks/use-color.ts
+++ src/hooks/use-color.ts
@@ -1,6 +1,7 @@
 import { useAtom } from 'jotai'
 import { atomWithStorage } from 'jotai/utils'
+import { themes, Theme } from '@common/themes'
 
-const configAtom = atomWithStorage( 'color', 'neutral' )
+const configAtom = atomWithStorage<Theme>( 'color', themes[ 0 ] )
 
 export const useColor = () => useAtom( configAtom )
