a2125108e3b1f336d3ecc7e7c1cef863e75d809c
Ganesh Basa <basaganesh1@gmail.com>
2024-07-31 23:22:34 +0530

    Modified Data

--- src/common/data.ts
+++ src/common/data.ts
@@ -1,15 +1,30 @@
+import { Project, Section } from '@common/interfaces'
 
-export const sections = [
-  // 'Hero',
-  'About',
-  // 'Resume',
-  // 'Approach',
-  // 'Projects',
-  // 'Skills',
-  // 'Technologies',
-  // 'Testimonials',
-  // 'Education',
-  // 'Work Experience',
-  // 'Contact',
-  // 'Footer'
+import { About, Projects } from '@components/section'
+
+
+export const sections : Section [] = [
+  {
+    name: 'about',
+    label: 'About',
+    component: About
+  },
+  {
+    name: 'projects',
+    label: 'Projects',
+    component: Projects
+  }
+]
+
+export const projects : Project [] = [
+  {
+    name: 'iris',
+    label: 'Iris',
+    description: 'Video Conferencing App'
+  },
+  {
+    name: 'hangman',
+    label: 'HangMan',
+    description: 'HangMan - Word Guessing Game'
+  }
 ]
